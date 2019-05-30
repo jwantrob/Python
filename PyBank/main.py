74429ca5b3d7f4b7ced74daf63dfc99214c7b3d4
Wantroba <Joseph.Wantroba@IRIworldwide.com>
2019-05-30 17:39:09 -0500

    add min/max

--- PyBank/main.py
+++ PyBank/main.py
@@ -38,16 +38,30 @@ with open(csvpath, newline='') as csvfile:
 
     #create a variable to hold monthly change
     total_change = 0
+    #create a dictionary to hold date and monthly change
+    date_monthly_change = {}
     #compare items in gain_loss list and list_date to find average change and greatest/least profit. Remember list index starts at 0.
     for i in range(1,len(list_gain_loss)):
         monthly_change = int(list_gain_loss[i]) - int(list_gain_loss[i-1])
         total_change = total_change + monthly_change
-    
+        #append to dictionary the monthly change by date calculated above.
+        date_monthly_change.update({list_date[i] : monthly_change})
+        #print(date_monthly_change)
     #create average monthly change by taking total change and dividing by length of list_gain_loss-1 (since we subtracted the values there is 1 less to divide from)
     average_change = total_change/(len(list_gain_loss)-1)
     #round to 2 decimal places
     round_average_change = round(average_change,2)
     print(f'Average Change: ${round_average_change}')
+    #find the min and max in dictionary date_monthly_change used: http://www.trytoprogram.com/python-programming/python-dictionary/#maxmin
+    monthly_max_date = max(date_monthly_change.keys(), key=(lambda a: date_monthly_change[a]))
+    monthly_max_value = date_monthly_change[monthly_max_date]
+    print(monthly_max_date)
+    print(monthly_max_value)
+    monthly_min_date = min(date_monthly_change.keys(), key=(lambda a: date_monthly_change[a]))
+    monthly_min_value = date_monthly_change[monthly_min_date]
+    print(monthly_min_date)
+    print(monthly_min_value)
+        
 
        
 
