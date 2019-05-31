294b8ae9005872e17613d69bcd57475729857c2b
Wantroba <Joseph.Wantroba@IRIworldwide.com>
2019-05-31 16:40:09 -0500

    final code

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,15 +5,15 @@ import csv
 
 #import csv
 
-csvpath = os.path.join('PyBank','budget_data.csv')
+csvpath = os.path.join('budget_data.csv')
 
 with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile,delimiter=',')
-    print(csvreader)
+    #print(csvreader)
 
     #print header information
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
+    #print(f"CSV Header: {csv_header}")
 
     #start a total_gain_loss variable = 0
     #start a total month variable = 0
@@ -33,6 +33,8 @@ with open(csvpath, newline='') as csvfile:
         list_gain_loss.append(row[1])
         list_date.append(row[0])
 #Print Total losses/gains and total months
+print('Financial Analysis')
+print('--------------------')
 print(f'Total Months: {total_month}')
 print(f'Total: ${total_gain_loss}')
 
@@ -64,13 +66,24 @@ monthly_max_date = max(date_monthly_change, key=date_monthly_change.get)
 #print(monthly_max_date_test)
 #then you can use that key to return the max/min value
 monthly_max_value = date_monthly_change[monthly_max_date]
-print(monthly_max_date)
-print(monthly_max_value)
+print(f'Greatest Increase in Profits: {monthly_max_date} (${monthly_max_value})')
 #monthly_min_date = min(date_monthly_change.keys(), key=(lambda a: date_monthly_change[a]))
 monthly_min_date = min(date_monthly_change, key = date_monthly_change.get)
 monthly_min_value = date_monthly_change[monthly_min_date]
-print(monthly_min_date)
-print(monthly_min_value)
+print(f'Greatest Decrease in Profits: {monthly_min_date} (${monthly_min_value})')
+
+#Write and print results to .txt file
+
+output_path = os.path.join('Pybank_Results.txt')
+
+with open(output_path, 'w') as txtfile:
+    txtfile.write('Financial Analysis\n')
+    txtfile.write('--------------------\n')
+    txtfile.write(f'Total Months: {total_month}\n')
+    txtfile.write(f'Total: ${total_gain_loss}\n')
+    txtfile.write(f'Greatest Increase in Profits: {monthly_max_date} (${monthly_max_value})\n')
+    txtfile.write(f'Greatest Decrease in Profits: {monthly_min_date} (${monthly_min_value})\n')
+print('complete')
         
 
        
